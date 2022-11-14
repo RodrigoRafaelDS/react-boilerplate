907fed720844622e8759cc92f526e17dcb74d6c3
Rodrigo Rafael <rodrigo.sousa-1@syngenta.com>
2022-11-14 16:01:18 -0300

    plop and error bundary

--- src/pages/dashboard/dashboard.container.tsx
+++ src/pages/dashboard/dashboard.container.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Dashboard: React.FC = () => {
-	return <div className=''>DASHBOARDd</div>
+	return <div className=''>DASHBOARD</div>
 }
 
 export default Dashboard
