14dcd7e262eefd61f6815fd619a3876876676dce
Rodrigo Rafael <rodrigo.sousa-1@syngenta.com>
2022-11-14 19:47:48 -0300

    add github actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ErrorBoundary } from './ErrorBundaryComponent'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<ErrorBoundary>
+			<App />
+		</ErrorBoundary>
 	</React.StrictMode>
 )
