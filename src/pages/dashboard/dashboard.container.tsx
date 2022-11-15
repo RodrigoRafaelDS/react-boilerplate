0c2680b3f07c9f573fe9f05187922c4aba5f2c77
Rodrigo Rafael <rodrigo.sousa-1@syngenta.com>
2022-11-14 21:40:24 -0300

    add lint rules

--- src/pages/dashboard/dashboard.container.tsx
+++ src/pages/dashboard/dashboard.container.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Dashboard: React.FC = () => {
-	return <div className=''>DASHBOARgD</div>
+	return <div className=''>DASHBOARD</div>
 }
 
 export default Dashboard
