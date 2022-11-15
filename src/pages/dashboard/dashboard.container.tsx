6100e6d460ab93ed0c26e243fe93ae5d0f90f48c
Rodrigo Rafael <rodrigo.sousa-1@syngenta.com>
2022-11-15 11:03:14 -0300

    add sonarCloud

--- src/pages/dashboard/dashboard.container.tsx
+++ src/pages/dashboard/dashboard.container.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Dashboard: React.FC = () => {
-	return <div className=''>DASHBOARD</div>
+	return <div className=''>DASHBOARD test</div>
 }
 
 export default Dashboard
