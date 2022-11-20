73ff95bc00db77d00fab44b42582e0704f7ed5b2
Rodrigo Rafael <rodrigo.sousa-1@syngenta.com>
2022-11-20 18:45:35 -0300

    add plaWrite tests

--- tests-examples/demo-todo-app.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test'
-
-test('test', async ({ page }) => {
-	await page.goto('http://127.0.0.1:5173/')
-	await page.getByText('Page 2').click()
-	await page.getByText('Dashboard').click()
-	await page.getByRole('main').getByText('Dashboard').dblclick()
-})
